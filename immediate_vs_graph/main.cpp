0bb1da3259ab7cf08367739f0c4979850a076223
atinfinity <dandelion1124@gmail.com>
2018-04-26 01:32:14 +0900

    fixed typo

--- immediate_vs_graph/main.cpp
+++ immediate_vs_graph/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char* argv[])
 {
 	if(argc != 3)
 	{
-		std::cerr << "Usage: " << argv[0] << "<left_image> <right_image>" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " <left_image> <right_image>" << std::endl;
 		return -1;
 	}
 
